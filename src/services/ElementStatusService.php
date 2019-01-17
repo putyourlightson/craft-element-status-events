72b41ec520a6e82a3416576165943acc40367162
Ben Croker <ben@putyourlightson.net>
2019-01-17 22:07:04 +0100

    1.2.0

--- src/services/ElementStatusService.php
+++ src/services/ElementStatusService.php
@@ -1,34 +1,15 @@
 <?php
 namespace putyourlightson\elementstatusevents\services;
 
-use Craft;
 use craft\base\Component;
 use craft\base\Element;
 use craft\events\ElementEvent;
 use craft\services\Elements;
-use craft\web\Response;
 use putyourlightson\elementstatusevents\behaviors\ElementStatusBehavior;
-use putyourlightson\elementstatusevents\events\ElementStatusesEvent;
 use yii\base\Event;
 
 class ElementStatusService extends Component
 {
-    // Constants
-    // =========================================================================
-
-    /**
-     * @event ElementStatusesEvent
-     */
-    const EVENT_ELEMENT_STATUSES_CHANGED = 'elementStatusesChanged';
-
-    // Properties
-    // =========================================================================
-
-    /**
-     * @var Element[]
-     */
-    private $_elementsChanged = [];
-
     // Public Methods
     // =========================================================================
 
@@ -64,18 +45,6 @@ class ElementStatusService extends Component
 
                 if ($element->statusChanged) {
                     $this->_elementsChanged[] = $element;
-                }
-            }
-        });
-
-        // After preparing response
-        Craft::$app->getResponse()->on(Response::EVENT_AFTER_PREPARE, function() {
-            if (!empty($this->_elementsChanged)) {
-                // Trigger a 'statusesChanged' event
-                if ($this->hasEventHandlers(self::EVENT_ELEMENT_STATUSES_CHANGED)) {
-                    $this->trigger(self::EVENT_ELEMENT_STATUSES_CHANGED, new ElementStatusesEvent([
-                        'elements' => $this->_elementsChanged,
-                    ]));
                 }
             }
         });
